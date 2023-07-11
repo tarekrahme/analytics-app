f7bb43b0307a732a729100759a1a617d90bf56d9
Tarek Rahme <rahmetarek@gmail.com>
2023-07-11 17:06:35 +0200

    fly.io

---
+++ config/initializers/redis.rb
@@ -0,0 +1,14 @@
+# config/initializers/redis.rb
+redis = Redis.new
+url = ENV["REDIS_URL"]
+
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  redis = Redis.new(:url => url)
+end
